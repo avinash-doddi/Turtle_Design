51da24feea3b6c32cab7fe64a843793fe34c183b
Doddi Avinash <76215983+avinash-doddi@users.noreply.github.com>
2021-03-19 23:21:19 +0530

    Update Spiral_Design_Using_Turlte.py

--- Spiral_Design_Using_Turlte.py
+++ Spiral_Design_Using_Turlte.py
@@ -8,7 +8,7 @@ trtl.speed(0)
 screen.bgcolor('black')
 
 for x in range(360):
-    trtl.pencolor(colors[x % 6]) ''' here we can decrease [x%6] to [x%5 or [x%4] to decrease the width of spiral'''
+    trtl.pencolor(colors[x % 6]) ''' here we can decrease [x%6] to [x%5] or [x%4] to decrease the width of spiral'''
     trtl.width(x / 5 + 1)
     trtl.forward(x)
     trtl.left(20)
