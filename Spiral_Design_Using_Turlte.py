985687230cad00f6ed6d8531fa7f34715492c364
Doddi Avinash <76215983+avinash-doddi@users.noreply.github.com>
2021-03-19 23:23:43 +0530

    Update Spiral_Design_Using_Turlte.py

--- Spiral_Design_Using_Turlte.py
+++ Spiral_Design_Using_Turlte.py
@@ -7,8 +7,8 @@ trtl = turtle.Turtle()
 trtl.speed(0)
 screen.bgcolor('black') """this is background colour"""
 
-for x in range(360):   #if we change this to 180 or 250 we would get pre-complleted spiral
-    trtl.pencolor(colors[x % 6]) """here we can decrease [x%6] to [x%5] or [x%4] to decrease the width of spiral"""
+for x in range(360):  """if we change this to 180 or 250 we would get pre-complleted spiral"""
+    trtl.pencolor(colors[x % 6]) #here we can decrease [x%6] to [x%5] or [x%4] to decrease the width of spiral
     trtl.width(x / 5 + 1)
     trtl.forward(x)
     trtl.left(20)
