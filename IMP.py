f683a17313653b0acc58b3eb554d318e274d89e8
Doddi Avinash <76215983+avinash-doddi@users.noreply.github.com>
2021-03-26 22:40:01 +0530

    Create IMP.py

---
+++ IMP.py
@@ -0,0 +1,11 @@
+"""to calculate the balance and balance, and delete 0.50 from balance and display balance"""
+a,b=input().split()
+a=float(a)
+b=float(b)
+c=a+0.50
+if c<=b and a%5==0:
+    
+    d=b-c
+    print("{:.2f}".format(d))
+else:
+    print("{:.2f}".format(b))
